1a0a0ccd8041ad09d1897ec65359693d485fa991
pandepic <39251913+pandepic@users.noreply.github.com>
2020-09-07 03:43:57 +1000

    .

---
+++ warmachine-linux/Assets/Py/gameplay_menu.py
@@ -0,0 +1,78 @@
+﻿class GameplayMenu:
+    def __init__(selt):
+        pass
+
+    def BuildMinerClicked(instance):
+        BuildMiner(None)
+
+    def BuildFighterClicked(instance):
+        BuildFighter(None)
+
+    def BuildBomberClicked(instance):
+        BuildBomber(None)
+
+    def BuildRepairShipClicked(instance):
+        BuildRepairShip(None)
+
+    def BuildMissileFrigateClicked(instance):
+        BuildMissileFrigate(None)
+
+    def BuildBeamFrigateClicked(instance):
+        BuildBeamFrigate(None)
+
+    def BuildSupportCruiserClicked(instance):
+        BuildSupportCruiser(None)
+
+    def BuildHeavyCruiserClicked(instance):
+        BuildHeavyCruiser(None)
+
+    def BuildBattleshipClicked(instance):
+        BuildBattleship(None)
+
+    def BuildCarrierClicked(instance):
+        BuildCarrier(None)
+
+    def IdleMinersClicked(instance):
+        IdleMiners(None)
+
+    def UpgradesClicked(instance):
+        Upgrades(None)
+
+    def BuildClicked(instance):
+        Build(None)
+
+    def HelpClicked(instance):
+        Help(None)
+
+    def UpgradeMinerCap1Clicked(instance):
+        UpgradeMinerCap1(None)
+
+    def UpgradeMinerCap2Clicked(instance):
+        UpgradeMinerCap2(None)
+
+    def UpgradeMiningRate1Clicked(instance):
+        UpgradeMiningRate1(None)
+
+    def UpgradeMiningRate2Clicked(instance):
+        UpgradeMiningRate2(None)
+
+    def UpgradeRepairRateClicked(instance):
+        UpgradeRepairRate(None)
+
+    def UpgradeShieldRegen1Clicked(instance):
+        UpgradeShieldRegen1(None)
+
+    def UpgradeShieldRegen2Clicked(instance):
+        UpgradeShieldRegen2(None)
+
+    def UpgradeWarmachine1Clicked(instance):
+        UpgradeWarmachine1(None)
+
+    def UpgradeWarmachine2Clicked(instance):
+        UpgradeWarmachine2(None)
+
+    def UpgradeHyperdriveClicked(instance):
+        UpgradeHyperdrive(None)
+
+    def ExitToMenuClicked(instance):
+        ExitToMenu(None)
